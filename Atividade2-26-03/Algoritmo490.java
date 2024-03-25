827c8a6d03265ca02ae9b3be11b0533d59db33da
João Paulo M. Silva <joaopauloescola212@gmail.com>
2024-03-25 20:41:33 -0300

    Alteração no Algortimo490

--- Atividade2-26-03/Algoritmo490.java
+++ Atividade2-26-03/Algoritmo490.java
@@ -3,19 +3,24 @@ import java.util.Scanner;
 public class Algoritmo490 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Digite uma palavra: ");
-        String inputWord = scanner.nextLine();
-        char[] palavra = inputWord.toCharArray();
-
+        
+        String[] palavras = new String[10];
+        char caracter;
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Digite uma palavra " + (i+1) + ": ");
+            palavras[i] = scanner.nextLine();
+        }
+        
         System.out.print("Digite o caractere que deseja remover: ");
-        char caracter = scanner.next().charAt(0);
-
-        int tam = palavra.length;
-        int removidos = caractere(palavra, tam, caracter);
-
-        System.out.println("Nova palavra: " + String.valueOf(palavra));
-        System.out.println("Total de caracteres removidos: " + removidos);
+        caracter = scanner.next().charAt(0);
+        
+        for (int i = 0; i < 10; i++) {
+            char[] palavra = palavras[i].toCharArray();
+            int removidos = caractere(palavra, palavra.length, caracter);
+            System.out.println("Palavra " + i + ": " + String.valueOf(palavra));
+            System.out.println("Total de caracteres removidos: " + removidos);
+        }
 
         scanner.close();
     }
@@ -32,5 +37,4 @@ public class Algoritmo490 {
 
         return substituidos;
     }
-
 }
